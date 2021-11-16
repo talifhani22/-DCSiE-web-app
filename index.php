2ee1a5b36bde298b5efef483b824f6ffca2b94c4
Given Makofane <makofane411@users.noreply.github.com>
2021-11-16 19:36:16 +0200

    addition of lib,css and js
    adding js script, css and other lib to work with for design and partial folder to reuse design

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+ include("partials/header.php")
+?>
+<body>
+    <h2>test</h2>
+
+</body>
+</html>
---
+++ partials/header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  
+        <meta charset="utf-8">
+        <title>Digital Campus Structures Inventory eMalahlanei</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Digital Campus Structures Inventory eMalahlanei " name="DCSIE">
+        <meta content="Recording and managing the campus infrastructre " name="Digital Campus Structures Inventory eMalahlanei">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Nunito:600,700" rel="stylesheet"> 
+        
+        <!-- CSS Libraries -->
+        <link href="/https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+        <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" >
+
+         <!-- Template Stylesheet -->
+         <link href="css/style.css" rel="stylesheet">
+
+  </head>
+
+
